082ae41766435c95c43f2500bb0ec98626c97843
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2021-05-02 15:41:04 -0300

    feat: show enterprise name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = auth()->user();
+        
+        $enterprise = $user->enterprises()->first();
+
+        return view('home', compact('enterprise'));
     }
 }
