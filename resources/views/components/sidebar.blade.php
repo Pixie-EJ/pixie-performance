7ed4c734fa275a33f5b28312e524940ad2b67d4a
edusaitto <saitoeduardo2001@gmail.com>
2021-07-03 15:31:48 -0300

    fix: sidebar mostrando pagina atual

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,13 +1,14 @@
 
 <div class="sidebar bg-black"> 
     <ul class="sidebar-list" style="padding: 0">
-        <a href="/home"><li class="color-light m-0 py-3 px-4 sidebar-item-1">Home</li></a>
-        <a href=""><li class="color-grey m-0 py-3 px-4 sidebar-item-2">Membros</li></a>
-        <a href=""><li class="color-grey m-0 py-3 px-4 sidebar-item-3">Equipes</li></a>
-        <a href="/events"><li class="color-light m-0 py-3 px-4 sidebar-item-4">Eventos</li></a>
-        <a href=""><li class="color-grey m-0 py-3 px-4 sidebar-item-5">Categorias</li></a>
-        <a href=""><li class="color-grey m-0 py-3 px-4 sidebar-item-6">Regras</li></a>
-        <a href=""><li class="color-grey m-0 py-3 px-4 sidebar-item-7">Emblemas</li></a>
-        <a href=""><li class="color-grey m-0 py-3 px-4 sidebar-item-8">Temporadas</li></a>
+        <a href="/home"><li class="color-light m-0 py-3 px-4 @if (request()->is('home')) bg-green @endif">Home</li></a>
+        <a href=""><li class="color-grey m-0 py-3 px-4">Membros</li></a>
+        <a href=""><li class="color-grey m-0 py-3 px-4">Equipes</li></a>
+        <a href="/events"><li class="color-light m-0 py-3 px-4 @if (request()->is('events')) bg-green @endif">Eventos</li></a>
+        <a href=""><li class="color-grey m-0 py-3 px-4">Categorias</li></a>
+        <a href=""><li class="color-grey m-0 py-3 px-4">Regras</li></a>
+        <a href=""><li class="color-grey m-0 py-3 px-4">Emblemas</li></a>
+        <a href=""><li class="color-grey m-0 py-3 px-4">Temporadas</li></a>
     </ul>    
-</div> 
+</div>
+
