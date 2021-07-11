3843193f1321997e4820f7788e9d939694d2ba81
OtavioPedroso <otavioaugustosantos59@gmail.com>
2021-07-11 09:31:05 -0300

    feat:listagem dos eventos na tela de lançamento de ponto

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Event;
 
 class ScoreController extends Controller
 {
@@ -23,7 +24,10 @@ class ScoreController extends Controller
      */
     public function create()
     {
-        return view('score.create');
+        $events = Event::query()
+            ->orderBy('started_at', 'desc')
+            ->get();
+        return view('score.create',compact('events'));
     }
 
     /**
